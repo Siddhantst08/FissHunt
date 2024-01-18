da04605f9325184fa4e73e54e361cdec5f602351
Siddhantst08 <siddhantst08@gmail.com>
2024-01-19 00:06:47 +0530

    some django changes

--- DjangoAPP/urls.py
+++ DjangoAPP/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from DjangoAPP import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("DjangoAPP.urls")),
+    path("", views.index, name="home"),
+   # path("login", views.loginUser, name = 'login')
 ]
--- FissHunt/settings.py
+++ FissHunt/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "DjangoAPP.apps.DjangoappConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
