5bbda44ab5fe07b6aa83fce8fa2294065084cd3d
Siddhantst08 <siddhantst08@gmail.com>
2024-01-22 23:36:08 +0530

    authentication successfull

--- Backend/views.py
+++ Backend/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
- 
+from django.views.decorators.csrf import ensure_csrf_cookie
+
+
 
 
 # Create your views here.
+def index3(request):
+    return render(request, 'index.html')
 
 def index(request):
     return render(request, 'index.html')
 
+@ensure_csrf_cookie
 def index2(request):
     
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("email")
         password = request.POST.get("password")
         print(username, password)
 
@@ -19,11 +24,13 @@ def index2(request):
 
         if user is not None:
             login(request, user)
+            print("hogyaaa")
             # A backend authenticated the credentials
             
-            return redirect('home.html')
+            return redirect('/exchange')
         else:
+            print("no hua")
             # No backend authenticated the credentials
-            return render(request, 'index.html')
+            return render(request, '/login')
 
-    return render(request, 'index.html')
+    return redirect('/login')
